a80cd5d6ad62b29b36cfc4a68d559e09ef81beaf
OluEkele <ekeleolu1@gmail.com>
2023-10-11 10:18:16 +0100

    deploy

--- script/pro.js
+++ script/pro.js
@@ -1,17 +1,15 @@
 // let slide = tns({
 //     container : ".body-slide",
-//     width : 100,
 //     "items" : 1,
-//     "scrollBy" : true,
 //     "slideBy" : "1",
-//     "speed" : 400,
+//     "speed" : 100,
 //     "nav" : false,
 //     autoplay : true,
 //     controls : false,
 //     autoplayButtonOutput : false,
 //     Responsive: {
 //        1600: {
-//         items : 3, 
+//         items : 4, 
 //         },
 //        1024: {
 //         items : 3,
@@ -30,38 +28,19 @@
 
 
 
-// const scroller = document.querySelector(".body-slide");
-// if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-//   addAmination();
-// }
-// function addAmination() {
-//   scroller.forEach((scroller) =>{
-//     scroller.setAttribute('data-animation', 'true');
-
-//     const scrollerImage = scroller.querySelector('.image-slide');
-//     const scrollerContent = Array.from(scroller.children);
-
-//     scrollerContent.forEach((item) => {
-//       const duplicateItem = item.cloneNode(true);
-//       duplicateItem.setAttribute("aria-hidden", "true");
-//       scrollerImage.appendChild(duplicateItem);
-//     });
-//   });
-// }
 
 
 
-
-var myVar;
+// var myVar;
         
-function myFunction() {
-    myVar = setTimeout(showPage, 3000);
-  }
+// function myFunction() {
+//     myVar = setTimeout(showPage, 3000);
+//   }
   
-  function showPage() {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("myDiv").style.display = "block";
-}    
+//   function showPage() {
+//     document.getElementById("loader").style.display = "none";
+//     document.getElementById("myDiv").style.display = "block";
+// }    
 
 
 
